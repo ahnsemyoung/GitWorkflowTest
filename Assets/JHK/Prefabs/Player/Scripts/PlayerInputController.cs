ee7c691bc772fdc7b4970fc601501d7065acff37
ju6579 <ju6579@naver.com>
2020-09-21 09:34:15 +0900

    Player Input Contoller 추가 및 초기작성
    *WASD 키 입력 Position 제어
    *마우스 축 입력 카메라 Rotation 제어
    
    -issue-
    # 카메라 Rotation X축 짐벌락 현상
    # 무제한 점프, Ground 판단 필요

---
+++ Assets/JHK/Prefabs/Player/Scripts/PlayerInputController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
+public class PlayerInputController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PlayerCamera = null;
+
+    [SerializeField]
+    private float PlayerMoveSpeed = 1.0f;
+
+    [SerializeField]
+    private float PlayerRotateSpeed = 60.0f;
+
+    [SerializeField]
+    private float JumpForce = 5.0f;
+
+    private CapsuleCollider _playerCollider = null;
+    private Rigidbody _playerRigidbody = null;
+
+    private float _playerVerticalRotate = 0.0f;
+    private float _playerHorizontalRotate = 0.0f;
+
+    #region MonoBehaviour Callbacks
+    private void Start()
+    {
+        _playerCollider = GetComponent<CapsuleCollider>();
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        if(PlayerCamera == null) 
+        { 
+            Debug.Log("Player Camera has Not Attached, Please Check Player Object");
+            gameObject.SetActive(false);
+        }
+
+        Vector3 playerEulerAngle = PlayerCamera.transform.rotation.eulerAngles;
+        _playerVerticalRotate = playerEulerAngle.x;
+        _playerHorizontalRotate = playerEulerAngle.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump")) JumpOnPosition();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 nextPosition = GetNextDirectionByKeyInput() * Time.fixedDeltaTime * PlayerMoveSpeed
+                            + _playerRigidbody.position;
+        _playerRigidbody.MovePosition(nextPosition);
+    }
+
+    private void LateUpdate()
+    {
+        SetPlayerRotationByMouseInput();
+    }
+    #endregion
+
+    #region Private Field
+    private Vector3 GetNextDirectionByKeyInput()
+    {
+        Vector3 inputVector = Vector3.zero;
+
+        inputVector += Input.GetAxisRaw("Horizontal") * PlayerCamera.transform.right;
+        inputVector += Input.GetAxisRaw("Vertical") * PlayerCamera.transform.forward;
+        inputVector.y = 0;
+
+        return inputVector.normalized;
+    }
+
+    private void SetPlayerRotationByMouseInput()
+    {
+        _playerVerticalRotate -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * PlayerRotateSpeed;
+        _playerHorizontalRotate += Input.GetAxisRaw("Mouse X") * Time.deltaTime * PlayerRotateSpeed;
+
+        Vector3 playerEulerAngle = PlayerCamera.transform.rotation.eulerAngles;
+        playerEulerAngle.x = _playerVerticalRotate;
+        playerEulerAngle.y = _playerHorizontalRotate;
+
+        PlayerCamera.transform.rotation = Quaternion.Euler(playerEulerAngle);
+    }
+
+    private void JumpOnPosition()
+    {
+        _playerRigidbody.AddForce(transform.up * _playerRigidbody.mass * JumpForce, ForceMode.Impulse);
+    }
+    #endregion
+}
